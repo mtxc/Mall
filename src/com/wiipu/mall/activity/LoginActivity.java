e8cbec114d684185a0ef8b16c78e9c4fe9d3a91b
mtxc <710649878@qq.com>
2015-06-18 16:28:25 +0800

    register

--- src/com/wiipu/mall/activity/LoginActivity.java
+++ src/com/wiipu/mall/activity/LoginActivity.java
@@ -12,12 +12,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+/**
+ * 注册和登录Activity
+ */
 public class LoginActivity extends Activity {
 
 	private ImageButton ibBack;
 	private EditText etAccount;
 	private EditText etPassword;
 	private Button btnLogin;
+	private Button btnRegister;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,9 @@ public class LoginActivity extends Activity {
 		initView();
 	}
 
+	/**
+	 * 初始化视图
+	 */
 	private void initView() {
 		ibBack = (ImageButton) findViewById(R.id.login_ib_back);
 		ibBack.setOnClickListener(new OnClickListener() {
@@ -49,8 +56,18 @@ public class LoginActivity extends Activity {
 			public void onClick(View v) {
 			}
 		});
+		btnRegister = (Button) findViewById(R.id.login_btn_register);
+		btnRegister.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+			}
+		});
 	}
 
+	/**
+	 * 监听文本框
+	 */
 	class MyWatcher implements TextWatcher {
 
 		@Override
@@ -59,8 +76,10 @@ public class LoginActivity extends Activity {
 			if ("".equals(etAccount.getText().toString())
 					|| "".equals(etPassword.getText().toString())) {
 				btnLogin.setEnabled(false);
+				btnRegister.setEnabled(false);
 			} else {
 				btnLogin.setEnabled(true);
+				btnRegister.setEnabled(true);
 			}
 		}
 
